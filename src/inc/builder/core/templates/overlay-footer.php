3eefd2048cbef7160da6ab54b44760f9c18475a1
sixFingers <ignazio.setti@gmail.com>
2017-01-17 09:15:02 +0100

    Restyle overlay following WordPress styles

--- src/inc/builder/core/templates/overlay-footer.php
+++ src/inc/builder/core/templates/overlay-footer.php
@@ -1,3 +1,6 @@
 		</div>
+		<div class="ttfmake-overlay-footer">
+			<button type="button" class="button media-button button-primary button-large media-button-insert ttfmake-overlay-close ttfmake-overlay-close-action"><?php esc_html_e( 'Done', 'make' ); ?></button>
+		</div>
 	</div>
 </div>
