cd1349a40e290de894f44c88940892917c850e54
sixFingers <ignazio.setti@gmail.com>
2017-01-21 15:33:34 +0100

    Frontend styling for different desktop/mobile menus

--- src/partials/header-layout.php
+++ src/partials/header-layout.php
@@ -68,23 +68,16 @@ $header_bar_menu = wp_nav_menu( array(
 				<span class="menu-toggle"><?php echo make_get_thememod_value( 'navigation-mobile-label' ); ?></span>
 				<?php
 				$mobile_menu = make_get_thememod_value( 'mobile-menu' );
-				$mobile_menu_class = 'menu menu-mobile';
 
-				if ( $mobile_menu === 'primary' ) {
-					wp_nav_menu( array(
-						'theme_location' => 'primary',
-						'menu_class' => $mobile_menu_class,
-					) );
-				} else {
-					wp_nav_menu( array(
-						'theme_location' => 'primary'
-					) );
+				wp_nav_menu( array(
+					'theme_location' => 'primary',
+					'menu_class' => 'menu menu-desktop',
+				) );
 
-					wp_nav_menu( array(
-						'theme_location' => $mobile_menu,
-						'menu_class' => $mobile_menu_class,
-					) );
-				}
+				wp_nav_menu( array(
+					'theme_location' => $mobile_menu,
+					'menu_class' => 'menu menu-mobile',
+				) );
 				?>
 			</nav>
 		</div>
