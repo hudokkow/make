45205d73074fcacdc5b522d70375a43e06d836a5
sixFingers <ignazio.setti@gmail.com>
2017-01-21 15:39:54 +0100

    Move alternate menu navigation logic to partial

--- src/partials/header-layout.php
+++ src/partials/header-layout.php
@@ -65,20 +65,7 @@ $header_bar_menu = wp_nav_menu( array(
 			</div>
 
 			<nav id="site-navigation" class="site-navigation" role="navigation">
-				<span class="menu-toggle"><?php echo make_get_thememod_value( 'navigation-mobile-label' ); ?></span>
-				<?php
-				$mobile_menu = make_get_thememod_value( 'mobile-menu' );
-
-				wp_nav_menu( array(
-					'theme_location' => 'primary',
-					'menu_class' => 'menu menu-desktop',
-				) );
-
-				wp_nav_menu( array(
-					'theme_location' => $mobile_menu,
-					'menu_class' => 'menu menu-mobile',
-				) );
-				?>
+				<?php get_template_part( 'partials/site-navigation' ); ?>
 			</nav>
 		</div>
 	</div>
